b23873cc3d3b7c1929191304e0ee6acdf0018034
chenpeng1919 <chen.peng1919@gmail.com>
2024-01-09 22:40:51 -0500

    user api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -113,9 +113,9 @@ class PrivateUserApiTests(TestCase):
 
     def setUp(self):
         self.user = create_user(
-            'email': 'test@example.com',
-            'password': 'testpass123',
-            'name': 'Test Name',
+            email='test@example.com',
+            password='testpass123',
+            name='Test Name',
         )
         self.client = APIClient()
         self.client.force_authenticate(user=self.user)
@@ -134,7 +134,7 @@ class PrivateUserApiTests(TestCase):
         """Test POST is not allowed for the me endpoint."""
         res = self.client.post(ME_URL, {})
 
-        self.assertEqual(res.status_code, status.HTTP_METHOD_NOT_ALLOWED)
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_update_user_profile(self):
         """Test updating the user profile for the autheticated user."""
@@ -144,5 +144,5 @@ class PrivateUserApiTests(TestCase):
 
         self.user.refresh_from_db()
         self.assertEqual(self.user.name, payload['name'])
-        self.assertEqual(self.user.check_password([payload['password']]))
+        self.assertTrue(self.user.check_password(payload['password']))
         self.assertEqual(res.status_code, status.HTTP_200_OK)
