2f4b0b783487c663387da95e82a05cd6dab7cb67
chenpeng1919 <chen.peng1919@gmail.com>
2024-01-10 11:12:15 -0500

    test recipe

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -94,7 +94,18 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_create_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example', 'password': 'pass123'}
+        res = self.client.post
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
     def test_create_token_blank_password(self):
+        """Test posting a blank password returns an error"""
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
